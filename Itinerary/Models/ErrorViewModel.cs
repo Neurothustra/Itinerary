72be44967934de8d308a66250e499d28aa859eff
bahern <william.ahern@gmail.com>
2017-10-18 19:11:29 -0400

    Add project files.

---
+++ Itinerary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Itinerary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
